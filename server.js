c129f5a02681c9cf116319dfb50c0984cb16451a
addiedp <b2028addpena@pshs.edu.ph>
2025-04-04 11:21:21 +0800

    Update server.js

--- server.js
+++ server.js
@@ -1,14 +1,29 @@
 // Loads the express module
 const express = require("express");
 const hbs = require("hbs");
-
 const bodyParser = require("body-parser");
-
 const path = require("path");
 
 //Creates our express server
 const app = express();
 const port = 3000;
+
+// Register Handlebars helpers
+hbs.registerHelper('startsWith', function(str, prefix) {
+  return str.toString().startsWith(prefix);
+});
+
+hbs.registerHelper('concat', function(str1, str2) {
+  return str1.toString() + str2.toString();
+});
+
+hbs.registerHelper('last', function(str) {
+  return str.slice(-1);
+});
+
+hbs.registerHelper('eq', function(a, b) {
+  return a === b;
+});
 
 //Serves static files (we need it to import a css file)
 app.use(express.static(path.join(__dirname, "public")));
@@ -16,31 +31,19 @@ app.set("view engine", "hbs");
 app.use(bodyParser.urlencoded({ extended: true }));
 
 //Sets a basic route
-
-// Render the initial page with the number input form
 app.get("/", (req, res) => {
   res.render("index");
 });
 
-// Create express route binder for draw.hbs and get the data from the url as parameters
-// that came from index.hbs
-
 app.post("/happy", (req, res) => {
-
   res.render("happy");
-
-})
-
-app.get("/happy", (req, res)=>{
-  const query = req.query;
-  //var string = JSON.stringify(query)
-  console.log(query)
-  //var obj = JSON.parse(query)
-  res.render("happy", {data : query});
 });
 
-
-
+app.get("/happy", (req, res) => {
+  const query = req.query;
+  console.log(query);
+  res.render("happy", {data: query});
+});
 
 //Makes the app listen to port 3000
 app.listen(port, () => console.log(`App listening to port ${port}`));
